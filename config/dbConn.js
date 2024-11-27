30afad68286588a1046800d2e1f5df47b3b11417
nestor2178 <nescampuzano@misena.edu.co>
2024-11-27 16:14:23 -0500

    Configurar CORS para permitir solicitudes desde el frontend desplegado

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
-    console.log('Conectado a la base de datos');
   } catch (err) {
     console.log(err);
   }
