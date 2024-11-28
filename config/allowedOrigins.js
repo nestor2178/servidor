2fa8c72108ca78e6dbb754ce9573e95fcb01924b
nestor2178 <nescampuzano@misena.edu.co>
2024-11-27 20:07:43 -0500

    cambio cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
     'https://cliente-8a4k.onrender.com',
     'http://localhost:8000',
     'http://localhost:3500',
-    'http://localhost',
 ];
 
 export default allowedOrigins;
