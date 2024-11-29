08e4bc4f2c84999270383b2352ebefd29bced78b
nestor2178 <nescampuzano@misena.edu.co>
2024-11-28 19:39:59 -0500

    cambios en config/allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'https://cliente-8a4k.onrender.com',
     'http://localhost:10000',
     'http://localhost:3500',
+    'https://servidor-7zli.onrender.com',
 ];
 
 export default allowedOrigins;
