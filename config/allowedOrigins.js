14ba1394c76c0bffce1b1c707333d35d5ff0936c
nestor2178 <nescampuzano@misena.edu.co>
2025-05-31 11:46:38 -0500

    Actualizacion del backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
 const allowedOrigins = [
-    "https://cliente-8a4k.onrender.com",
-    "https://servidor-7zli.onrender.com",
-    "http://localhost:10000",
-    "http://localhost:3500",
-    "http://localhost:8000"
+  "https://cliente-8a4k.onrender.com",
+  "https://servidor-7zli.onrender.com",
+  "http://localhost:10000",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "http://localhost:8000",
 ];
 
 export default allowedOrigins;
