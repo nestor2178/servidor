dec094aad756ffae88b2a6fead9518cd4d1acf7d
nestor2178 <140920120+nestor2178@users.noreply.github.com>
2025-05-31 12:02:42 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://cliente-8a4k.onrender.com",
   "https://servidor-7zli.onrender.com",
+  "https://cliente-phi.vercel.app",
   "http://localhost:10000",
   "http://localhost:3500",
   "http://localhost:3000",
